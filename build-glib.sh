4231dc74703769a4ca3750963e9db4cd9ff7d95d
TheAssassin <theassassin@assassinate-you.net>
2018-04-11 05:25:38 +0200

    Install into different tree

--- build-glib.sh
+++ build-glib.sh
@@ -9,11 +9,11 @@ cd glib-$GLIB_VERSION
 # required to make the compiler happy
 export CFLAGS="$CFLAGS -Wno-error"
 
-EXTRA_CONFIGURE_FLAGS=" --libdir=/usr/lib64"
+EXTRA_CONFIGURE_FLAGS=
 if [ "$ARCH" == "i386" ]; then
     EXTRA_CONFIGURE_FLAGS=" --build=i686-linux-gnu --host=i686-linux-gnu --target=i686-linux-gnu"
 fi
-./configure --prefix=/usr $EXTRA_CONFIGURE_FLAGS
+./configure --prefix=/deps $EXTRA_CONFIGURE_FLAGS
 
 set +x
 echo "+ make -j$(nproc)" 1>&2
@@ -29,12 +29,6 @@ fi
 set -x
 
 make install
-
-# pkg-config files are installed into the wrong location, at least on CentOS 6
-# the following check fixes this issue
-if [ -d /usr/share/pkgconfig ]; then
-    mv /usr/lib/pkgconfig/* /usr/share/pkgconfig
-fi
 
 cd ../../
 rm -rf glib-$GLIB_VERSION/
