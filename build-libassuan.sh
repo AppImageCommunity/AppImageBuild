ca8b954154902ad81bd20fc1669bf32d64a16c00
TheAssassin <theassassin@assassinate-you.net>
2022-07-21 04:16:45 +0200

    Build libgcrypt and gpgme, mk. 4

---
+++ build-libassuan.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+set -xe
+
+git clone https://dev.gnupg.org/source/libassuan.git -b libassuan-"$LIBASSUAN_VERSION" libassuan-"$LIBASSUAN_VERSION"
+
+cd libassuan-"$LIBASSUAN_VERSION"
+
+# needed for automake
+export PATH=/deps/bin:"$PATH"
+
+EXTRA_CONFIGURE_FLAGS=
+if [ "$ARCH" == "i386" ]; then
+    EXTRA_CONFIGURE_FLAGS=" --build=i686-linux-gnu --host=i686-linux-gnu --target=i686-linux-gnu"
+elif [ "$ARCH" == "armhf" ] || [ "$ARCH" == "aarch64" ]; then
+    EXTRA_CONFIGURE_FLAGS=" --host=$DEBARCH --target=$DEBARCH"
+fi
+
+[ -f autogen.sh ] && ./autogen.sh
+
+./configure --prefix=/deps $EXTRA_CONFIGURE_FLAGS
+
+set +x
+echo "+ make -j$(nproc)" 1>&2
+
+if [ -z $VERBOSE ]; then
+    make -j$(nproc) 2>&1 | while read line; do
+        echo -n .
+    done
+    echo
+else
+    make -j$(nproc)
+fi
+set -x
+
+make install
+
+cd ../../
+rm -rf libassuan-"$LIBASSUAN_VERSION"/
