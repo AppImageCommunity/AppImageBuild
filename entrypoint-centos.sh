afd88af48c13d19f1743ffc9250b8106f86b713e
TheAssassin <theassassin@assassinate-you.net>
2022-08-28 03:25:11 +0200

    Move to devtoolset-9
    Allows the use of C++17 features like std::filesystem in projects like
    libappimage, AppImageUpdate.

--- entrypoint-centos.sh
+++ entrypoint-centos.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-source /opt/rh/devtoolset-8/enable
+source /opt/rh/devtoolset-9/enable
 
 export PATH=/deps/bin/:"$PATH"
 export PKG_CONFIG_PATH=/deps/lib/pkgconfig:"$PKG_CONFIG_PATH"
