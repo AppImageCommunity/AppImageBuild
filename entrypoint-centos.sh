5100790d827cb746079f2c2f4481baf509c51818
TheAssassin <theassassin@assassinate-you.net>
2023-07-13 15:17:33 +0200

    Update to devtoolset-10

--- entrypoint-centos.sh
+++ entrypoint-centos.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-source /opt/rh/devtoolset-9/enable
+source /opt/rh/devtoolset-10/enable
 
 export PATH=/deps/bin/:"$PATH"
 export PKG_CONFIG_PATH=/deps/lib/pkgconfig:"$PKG_CONFIG_PATH"
